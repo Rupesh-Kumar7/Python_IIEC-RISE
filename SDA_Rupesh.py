a7c7a65788d52d061c6a05202e913dedc1f181b1
Rupesh Kumar <69805812+Rupesh-Kumar7@users.noreply.github.com>
2020-08-20 14:19:39 +0530

    Add files via upload

--- SDA_Rupesh.py
+++ SDA_Rupesh.py
@@ -4,7 +4,7 @@ import os
 pt.speak("  Welcome!")
 pt.speak("Type what can i open for you from the below list")
 print("\n  Welcome!")
-print("\n->Chrome\n->Microsift-edge\n->Notepad\n->VSCode\n->Media_player\n->Music_player\n->Power Point\n->MS Word\n->MS Excel\n->Paint\n->Calculator\n->Camera\n->File Explorer\n->System Settings\n")
+print("\n->Chrome\n->Microsift-edge\n->Notepad\n->VSCode\n->Media_player\n->Music_player\n->Power Point\n->MS Word\n->MS Excel\n->Paint\n->Calculator\n->Camera\n->Calender\n->Alarm-Clock\n->Maps\n->News\n->Game-Candy Crush\n->Screensnip\n->File Explorer\n->System Settings\n")
 
 
 
@@ -16,7 +16,7 @@ while True:
 		pt.speak("Enter the next command!")
 		print("Enter the next command!")
 
-	else:
+	else: 
 		if ("chrome" in a):
 			os.system("start chrome")
 
@@ -50,8 +50,26 @@ while True:
 		elif("paint" in a):
 			os.system("start mspaint")
 
+		elif("alarm" in a) or ("clock" in a):
+			os.system("start ms-clock:")
+
+		elif("calender" in a):
+			os.system("start outlookcal:")
+
+		elif("maps" in a) or ("map" in a):
+			os.system("start ms-drive-to:")
+
+		elif("news" in a):
+			os.system("start bingnews:")
+
+		elif("candy crush" in a) or ("game" in a):
+			os.system("start candycrushsodasaga:")
+
 		elif(("vscode" in a) or ("code" in a)):
 			os.system("start code")
+
+		elif("snip" in a):
+			os.system("start snippingtool")
 
 		elif("file explorer" in a):
 			os.system("start explorer")
@@ -61,6 +79,7 @@ while True:
 
 		elif("exit" in a)  or ("quit" in a) or ("stop" in a):
 			pt.speak("Signing off, Bye!")
+			print("\n  Signing off, Bye!")
 			break
 		else:
 			pt.speak("Didn't undertand, please try again")
